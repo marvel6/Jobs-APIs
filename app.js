6a3a7d3b41c3062995b3f068215e9aca127e88ab
marvel6 <marvelloussolomon49@gmail.com>
2022-09-12 17:36:43 -0700

    swagger update

--- app.js
+++ app.js
@@ -43,11 +43,11 @@ app.use('/api/v1',route)
 app.use('/api/v1',authentication,routes)
 
 app.get('/', (req, res) => {
-  res.send('<h1>Jobs API</h1><a href="/Api-docs">Documetation</a>');
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documetation</a>');
 });
 
 
-app.use('/Api-docs',swagger.serve,swagger.setup(swaggerContent))
+app.use('/api-docs',swagger.serve,swagger.setup(swaggerContent))
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
